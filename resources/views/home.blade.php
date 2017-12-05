adc77edfab086eb136b893ccf114bef3fdf8ff52
Natalia Segura <nati.segura87@gmail.com>
2017-12-05 17:34:10 -0300

    Laravel - con login

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Calendario</div>
+
+                <div class="panel-body">
+                    <h1 style=""> PROXIMAMENTE</h1>
+                    <h2> Calendario para sacar turnos </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
